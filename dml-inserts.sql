0bfed2c28901f5407060cf6b3845dffb706862ba
Garcez0506 <109998303+Garcez0506@users.noreply.github.com>
2022-09-16 21:28:01 -0300

    Add files via upload

---
+++ dml-inserts.sql
@@ -0,0 +1,56 @@
+use vitrine_virtual;
+
+
+INSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Nike", "Tenis Running", "Tenis", 43, 129.9, 69.9);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Adidas", "Ultra Boost", "Performance", 40, 329.9, 220.9);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Puma", "Basics Run", "Basicos", 38, 139.9, 79.9);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Skechers", "Go Run", "Intermediario", 40, 299.9, 139.9);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Nike", "Vetor", "Performance", 42, 597.90, 259.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Puma", "Mandrake", "Basicos", 39, 129.9, 79.9);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Adidas", "Veloster", "Performance", 41, 999.90, 499.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Skechers", "Go Walk", "Basicos", 37, 229.90, 129.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Adidas", "Max Run", "Intermediario", 40, 395.90, 159.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Nike", "Runner Go", "Performance", 39, 899.90, 459.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Fila", "Racer", "Intermediario", 40, 499.90, 230.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Fila", "KR4", "Performance", 43, 699.90, 389.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Puma", "CityRun", "Basicos", 39, 299.90, 139.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Fila", "TR32", "Performance", 41, 799.90, 489.90);
+
+iNSERT INTO PRODUTOS (Marca, Nome, Cartegoria, Tamanho, Preco_Venda, Preco_Custo) 
+values ("Adidas", "Fusion", "Intermediario", 40, 479.90, 279.90);
+
+
+
+
+
+
+
+
+
---
+++ dml-updates.sql
@@ -0,0 +1,1 @@
+SELECT * FROM vitrine_virtual.produtos;
